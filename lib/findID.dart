4e5948c7729aa33562800dd426d7ba762370dc47
hanajjang25 <hanajjang25@naver.com>
2024-03-27 10:11:52 +0900

    project

--- lib/findID.dart
+++ lib/findID.dart
@@ -17,6 +17,7 @@ class findID extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
+                      SizedBox(height: 20),
                       Text(
                         '아이디 찾기',
                         style: TextStyle(
@@ -34,12 +35,11 @@ class findID extends StatelessWidget {
                           color: Colors.black,
                         ),
                       ),
-                      SizedBox(height: 20),
+                      SizedBox(height: 50),
                       TextField(
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
-                          labelText: 'ID',
-                          hintText: 'Enter your ID',
+                          labelText: '이름',
                         ),
                       ),
                       SizedBox(height: 15),
@@ -47,58 +47,10 @@ class findID extends StatelessWidget {
                         obscureText: true,
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
-                          labelText: 'Password',
-                          hintText: 'Enter secure password',
+                          labelText: '전화번호',
                         ),
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment
-                            .center, // 가운데 정렬을 위한 MainAxisAlignment 설정
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                            ),
-                            child: Text('로그인'),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        '아이디 저장',
-                        style: TextStyle(fontSize: 10),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        'Forgot ID?',
-                        style: TextStyle(
-                            fontSize: 10,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.blue,
-                            color: Colors.blue),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        'Forgot Password?',
-                        style: TextStyle(
-                            fontSize: 10,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.blue,
-                            color: Colors.blue),
                       ),
                       SizedBox(height: 20),
-                      Text(
-                        'Waitez가 처음이신가요?',
-                        style: TextStyle(
-                          fontSize: 10,
-                        ),
-                      ),
-                      SizedBox(height: 10),
                       Row(
                         mainAxisAlignment: MainAxisAlignment
                             .center, // 가운데 정렬을 위한 MainAxisAlignment 설정
@@ -111,10 +63,21 @@ class findID extends StatelessWidget {
                                 borderRadius: BorderRadius.circular(5),
                               ),
                             ),
-                            child: Text('회원가입'),
+                            child: Text('아이디 찾기'),
                           ),
                         ],
                       ),
+                      SizedBox(height: 20),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 2), // 양쪽에 50의 여백 추가
+                        child: Container(
+                          height: 1,
+                          width: double.maxFinite,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 150),
                     ],
                   ),
                 ),
